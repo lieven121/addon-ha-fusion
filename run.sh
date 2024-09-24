830d50e23caf3cdf0484541de1d7b665262e9cc6
lieven121 <ldetroch@outlook.com>
2024-09-24 21:25:36 +0200

    Ugh home assitant documentation and line cuts are SOOOO clear

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/with-contenv bashio
 
 # export HASS_PORT=$(bashio::core.port)
-export HASS_PORT=$(bashio::addon.option.hass_port)
+export HASS_PORT=$(bashio::addon.option "hass_port" )
+export HASS_URL=$(bashio::addon.option "hass_url")
 export EXPOSED_PORT=$(bashio::addon.port "8099/tcp")
-export HASS_URL=$(bashio::addon.option.hass_url)
 
 echo "Starting Fusion..."
 
