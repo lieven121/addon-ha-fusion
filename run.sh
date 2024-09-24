f0c38c7e72f87961d4e02a4cf1a0c2db6d0288f8
lieven121 <ldetroch@outlook.com>
2024-09-24 21:13:14 +0200

    add params to run

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/with-contenv bashio
 
-export HASS_PORT=$(bashio::core.port)
+# export HASS_PORT=$(bashio::core.port)
+export HASS_PORT=$(bashio::addon.option.hass_port bashio::core.port)
 export EXPOSED_PORT=$(bashio::addon.port "8099/tcp")
+export HASS_URL=$(bashio::addon.option.hass_url)
 
 echo "Starting Fusion..."
 
