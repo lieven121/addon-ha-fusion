1a246d94ad819847e5a751b2291960444a2722ad
lieven121 <ldetroch@outlook.com>
2024-09-24 21:19:50 +0200

    Add options with default value

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv bashio
 
 # export HASS_PORT=$(bashio::core.port)
-export HASS_PORT=$(bashio::addon.option.hass_port bashio::core.port)
+export HASS_PORT=$(bashio::addon.option.hass_port)
 export EXPOSED_PORT=$(bashio::addon.port "8099/tcp")
 export HASS_URL=$(bashio::addon.option.hass_url)
 
