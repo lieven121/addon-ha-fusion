9f3c200a1b686865857ee02d52adfb80072e4098
lieven121 <ldetroch@outlook.com>
2024-09-24 23:43:05 +0200

    Another option

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/with-contenv bashio
 
 # export HASS_PORT=$(bashio::core.port)
-export HASS_PORT=$(bashio::addon.option "hass_port" )
-export HASS_URL=$(bashio::addon.option "hass_url")
+export HASS_PORT=$(bashio::config "hass_port" )
+export HASS_URL=$(bashio::config "hass_url")
 export EXPOSED_PORT=$(bashio::addon.port "8099/tcp")
 
 echo "HASS_PORT: $HASS_PORT"
