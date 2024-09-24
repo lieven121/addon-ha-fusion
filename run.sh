c76a07c24eb5016e78b5190e41e05f9ca6e102eb
lieven121 <ldetroch@outlook.com>
2024-09-24 21:30:32 +0200

    Echo

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@ export HASS_PORT=$(bashio::addon.option "hass_port" )
 export HASS_URL=$(bashio::addon.option "hass_url")
 export EXPOSED_PORT=$(bashio::addon.port "8099/tcp")
 
+echo "HASS_PORT: $HASS_PORT"
+echo "HASS_URL: $HASS_URL"
+echo "EXPOSED_PORT: $EXPOSED_PORT"
 echo "Starting Fusion..."
 
 node server.js
