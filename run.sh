62a57913164ada096d810bb2920ab684e494fc6f
Lieven De Troch <ldetroch@outlook.com>
2024-09-26 20:22:08 +0200

    Add run as addon option

--- run.sh
+++ run.sh
@@ -3,11 +3,12 @@
 # export HASS_PORT=$(bashio::core.port)
 export HASS_PORT=$(bashio::config "hass_port" )
 export HASS_URL=$(bashio::config "hass_url")
+export ADDON=$(bashio::config "run_as_addon")
 export EXPOSED_PORT=$(bashio::addon.port "8099/tcp")
 
 echo "HASS_PORT: $HASS_PORT"
 echo "HASS_URL: $HASS_URL"
 echo "EXPOSED_PORT: $EXPOSED_PORT"
-# echo "Starting Fusion..."
+echo "Starting Fusion..."
 
 node server.js
